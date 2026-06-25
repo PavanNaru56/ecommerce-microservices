de6d803c3bfc40ef937ee3084219714aa652e5e2
Pavan Naru <pavannaru56@gmail.com>
2026-06-25 19:46:57 +0530

    DTO applied

---
+++ src/main/java/com/user/service/user_service/DTO/UserRequest.java
@@ -0,0 +1,4 @@
+package com.user.service.user_service.DTO;
+
+public class UserRequest {
+}
---
+++ src/main/java/com/user/service/user_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.user.service.user_service.exception;
+
+public class GlobalExceptionHandler {
+}
