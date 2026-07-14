6e366f2e94f759ddef81f8d097e9a9f79cd2625f
Pavan Naru <pavannaru56@gmail.com>
2026-07-14 23:09:59 +0530

    kafka integration

---
+++ kafka-notification/src/main/java/com/kafka_notification/kafka_notification/KafkaNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.kafka_notification.kafka_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaNotificationApplication.class, args);
+	}
+
+}
---
+++ kafka-notification/src/test/java/com/kafka_notification/kafka_notification/KafkaNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka_notification.kafka_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
